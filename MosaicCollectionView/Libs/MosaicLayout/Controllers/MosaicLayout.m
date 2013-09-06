c353cd538254c645544eb29374fed26758de781c
Kátai Imre <Meta@Katais-MacBook-Air.local>
2013-09-07 01:36:03 +0200

    Update layout to handle the double items as a mandatory directive not as an optional one

--- MosaicCollectionView/Libs/MosaicLayout/Controllers/MosaicLayout.m
+++ MosaicCollectionView/Libs/MosaicLayout/Controllers/MosaicLayout.m
@@ -35,6 +35,21 @@
     return retVal;
 }
 
+-(NSUInteger)largestColumnIndex{
+    NSUInteger retVal = 0;
+    CGFloat shortestValue = 0;
+    
+    NSUInteger i=0;
+    for (NSNumber *heightValue in _columns){
+        if ([heightValue floatValue] > shortestValue){
+            shortestValue = [heightValue floatValue];
+            retVal = i;
+        }
+        i++;
+    }
+    return retVal;
+}
+
 -(NSUInteger)longestColumnIndex{
     NSUInteger retVal = 0;
     CGFloat longestValue = 0;
@@ -79,7 +94,6 @@
 }
 
 #pragma mark UICollectionViewLayout
-
 -(void)prepareLayout{
     
     //  Set all column heights to 0
@@ -92,44 +106,74 @@
     NSUInteger itemsCount = [[self collectionView] numberOfItemsInSection:0];
     _itemsAttributes = [NSMutableArray arrayWithCapacity:itemsCount];
     
-    for (NSUInteger i = 0; i < itemsCount; i++){
-        NSIndexPath *indexPath = [NSIndexPath indexPathForItem:i inSection:0];
-        
-        //  Get x, y, width and height for indexPath
-        NSUInteger columnIndex = [self shortestColumnIndex];
-        NSUInteger xOffset = columnIndex * [self columnWidth];
-        NSUInteger yOffset = [[_columns objectAtIndex:columnIndex] integerValue];
-
-        NSUInteger itemWidth = 0;
-        NSUInteger itemHeight = 0;
-        float itemRelativeHeight = [self.delegate collectionView:self.collectionView relativeHeightForItemAtIndexPath:indexPath];
-        
-        if ([self canUseDoubleColumnOnIndex:columnIndex] &&
-            [self.delegate collectionView:self.collectionView isDoubleColumnAtIndexPath:indexPath]){
-            
-            itemWidth = [self columnWidth] * 2;
-            itemHeight = itemRelativeHeight * itemWidth;
-            itemHeight = itemHeight - (itemHeight % kHeightModule);            
-            
-            //  Set column height
-            _columns[columnIndex] = @(yOffset + itemHeight);
-            _columns[columnIndex+1] = @(yOffset + itemHeight);
-
-        }else{
-            itemWidth = [self columnWidth];
-            itemHeight = itemRelativeHeight * itemWidth;
-            itemHeight = itemHeight - (itemHeight % kHeightModule);            
-            
-            //  Set column height
-            _columns[columnIndex] = @(yOffset + itemHeight);
+    int i = 0;
+    NSMutableArray *skippedArray = [NSMutableArray array];
+    while ([_itemsAttributes count] != itemsCount) {
+        NSIndexPath *indexPath;
+        if (arc4random() % 2 == 0 && i < itemsCount) {
+            indexPath = [NSIndexPath indexPathForItem:i inSection:0];
+            i++;
+        } else {
+            indexPath = [skippedArray lastObject];
+            if ([_itemsAttributes count] + [skippedArray count] == itemsCount) {
+                [self normalizeColumns];
+            }
+            [skippedArray removeLastObject];
+            if (!indexPath &&  i < itemsCount) {
+                indexPath = [NSIndexPath indexPathForItem:i inSection:0];
+                i++;
+            }
         }
         
-        /*  Assign all those values to an UICollectionViewLayoutAttributes instance
-         *  and save it on an array */
-        UICollectionViewLayoutAttributes *attributes = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
-        attributes.frame = CGRectMake(xOffset, yOffset, itemWidth, itemHeight);
-        [_itemsAttributes addObject:attributes];
+        UICollectionViewLayoutAttributes *attributes = [self layoutAttributesForIndexPath:indexPath];
+        if (attributes) {
+            [_itemsAttributes addObject:attributes];
+        } else {
+            [skippedArray addObject:indexPath];
+        }
+        
     }
+}
+
+-(void)normalizeColumns {
+    NSUInteger columnIndex = [self shortestColumnIndex];
+    NSUInteger largestColumnIndex = [self largestColumnIndex];
+    _columns[columnIndex] = @([_columns[largestColumnIndex] floatValue]);
+}
+
+-(UICollectionViewLayoutAttributes *)layoutAttributesForIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewLayoutAttributes *attributes;
+    NSUInteger columnIndex = [self shortestColumnIndex];
+    NSUInteger xOffset = columnIndex * [self columnWidth];
+    NSUInteger yOffset = [[_columns objectAtIndex:columnIndex] integerValue];
+    
+    NSUInteger itemWidth = 0;
+    NSUInteger itemHeight = 0;
+    
+    float itemRelativeHeight = [self.delegate collectionView:self.collectionView relativeHeightForItemAtIndexPath:indexPath];
+    
+    if ([self.delegate collectionView:self.collectionView isDoubleColumnAtIndexPath:indexPath]){
+        if ([self canUseDoubleColumnOnIndex:columnIndex]) {
+            itemWidth = [self columnWidth] * 2;
+            itemHeight = itemRelativeHeight * itemWidth;
+            itemHeight = itemHeight - (itemHeight % kHeightModule);
+            
+            _columns[columnIndex] = @(yOffset + itemHeight);
+            _columns[columnIndex+1] = @(yOffset + itemHeight);
+            attributes = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
+            attributes.frame = CGRectMake(xOffset, yOffset, itemWidth, itemHeight);
+        }
+    } else {
+        itemWidth = [self columnWidth];
+        itemHeight = itemRelativeHeight * itemWidth;
+        itemHeight = itemHeight - (itemHeight % kHeightModule);
+        
+        _columns[columnIndex] = @(yOffset + itemHeight);
+        
+        attributes = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
+        attributes.frame = CGRectMake(xOffset, yOffset, itemWidth, itemHeight);
+    }
+    return attributes;    
 }
 
 -(NSArray *)layoutAttributesForElementsInRect:(CGRect)rect{    
